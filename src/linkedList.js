8407f8e66b222c19c700c5ff760eb3fca3ee8561
Hack Reactor Students <github@hackreactor.com>
2013-08-09 11:53:16 -0700

    rewrite .addToTail with Ryan for practice

--- src/linkedList.js
+++ src/linkedList.js
@@ -5,14 +5,20 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-
+    var newNode = makeNode(value);
+      if (list.head === null){
+        list.head = list.tail = newNode;
+        //list.head.next = null;
+      }else{
+         list.head.next = newNode;
+         list.tail = newNode;
+      }
   };
 
   list.removeHead = function(){
   };
 
   list.contains = function(value){
-    
   };
 
   return list;
