b8723c401614fdac4d902f312ea8ddd31a76fb18
Hack Reactor Students <github@hackreactor.com>
2013-08-09 08:30:24 -0700

    .addTail now: adds a node to the head when there is no head adds a node to the tail when there is no head adds a second node to the tail when there is already a tail moves pointers accordingly

--- src/linkedList.js
+++ src/linkedList.js
@@ -5,6 +5,14 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (list.head === null) {
+      list.head = list.tail = newNode;
+      list.tail.next = null;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
