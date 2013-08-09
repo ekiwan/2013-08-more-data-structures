a8d10a61907002f4f9d28ad2943a47b86bd6685f
Hack Reactor Students <github@hackreactor.com>
2013-08-09 11:28:47 -0700

    delete all code and rewrite for Ryan

--- src/linkedList.js
+++ src/linkedList.js
@@ -5,27 +5,14 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    var newNode = makeNode(value);
-    if (list.head === null) {
-      list.head = list.tail = newNode;
-      list.tail.next = null;
-    } else {
-      list.tail.next = newNode;
-      list.tail = newNode;
-    }
+
   };
 
   list.removeHead = function(){
   };
 
   list.contains = function(value){
-    var result = false;
-    for (var current = list.head; current !== null; current = current.next) {
-      if (current.value === value) {
-        result = true;
-      }
-    }
-    return result;
+    
   };
 
   return list;
